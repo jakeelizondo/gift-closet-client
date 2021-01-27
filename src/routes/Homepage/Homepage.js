f8998db4f7fda843a7410204566a72bce2440316
Jake Elizondo <jake.elizondo23@gmail.com>
2021-01-27 10:25:30 -0500

    homepage style edits from feedback

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -9,9 +9,12 @@ export default function Homepage() {
   return (
     <div className="homepage">
       <section className="hero white-background hero-1">
-        <h2>Gift. Smarter!</h2>
-        <div>
+        <div className="hero-image">
           <img src={giftMemoryIcon} alt={'gift-memory-icon'} />
+        </div>
+        <div className="hero-content">
+          <h2>Gift. Smarter!</h2>
+
           <p>
             Always forgetting gift ideas for friends or loved ones? Not anymore!
             With Gift Closet you can save gift ideas throughout the year so that
@@ -21,20 +24,25 @@ export default function Homepage() {
         </div>
       </section>
       <section className="hero" style={{ 'background-color': '#f941443b' }}>
-        <h2>Tag your gifts for easy access</h2>
-        <div>
+        <div className="hero-content">
+          <h2>Tag your gifts for easy access</h2>
           <p>
             Organize your gifts by person, event, holiday, you pick! Later,
             filter your gifts by your custom tags to see only the gift ideas you
             need for the occasion.
           </p>
+        </div>
+        <div className="hero-image">
           <img src={giftTagIcon} alt={'gift-tag-icon'} />
         </div>
       </section>
       <section className="hero white-background">
-        <h2>Manage your gift lists throughout the year</h2>
-        <div>
+        <div className="hero-image">
           <img src={giftListIcon} alt={'gift-tag-icon'} />
+        </div>
+        <div className="hero-content">
+          <h2>Manage your gift lists throughout the year</h2>
+
           <p>
             Create, update, or delete gifts and personal tags as often as you'd
             like to keep your lists up to date. Add website links, price info,
